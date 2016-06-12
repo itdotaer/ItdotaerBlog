5ec8d2540127747d7a7bd475f93c5d9bfd150ff1
Itdotaer <hujiangtao1235@gmail.com>
2016-06-12 15:51:12 +0800

    合并master分支

--- app/pages/about.js
+++ app/pages/about.js
@@ -2,22 +2,12 @@
  * About
  */
 var React = require('react');
-
-<<<<<<< HEAD
-var About = React.createClass({
-    render: function(){
-        return (
-            <div>
-                About Me
-            </div>
-=======
 var AboutComponent = require('../components/about');
 
 var About = React.createClass({
     render: function(){
         return (
             <AboutComponent />
->>>>>>> master
         );
     }
 });
