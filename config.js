58e1443d6719443d8d1e4bcd82e246c6e4fc6509
胡江涛 <jt_hu@Ctrip.com>
2016-08-11 14:32:12 +0800

    优化webpack打包js资源

--- config.js
+++ config.js
@@ -3,25 +3,25 @@
  */
 
 var appInfo = {
-    appName: 'Itdotaer Blog',
-    shortDescription: 'By Harry',
-    author: 'Harry Hu',
-    email: 'hujiangtao1235@qq.com',
-    buildDev: false,
-    isDebug: false,
-    hotPostNum: 5,
-    sessionSecret: 'itdotaer',
-    hash: '0a441586845211ad05f0',
-    apiUrl: 'http://itdotaerblog.herokuapp.com/api',
-    gitHubRepo: 'http://github.com/Itdotaer/ItdotaerBlog'
+  appName: 'Itdotaer Blog',
+  shortDescription: 'By Harry',
+  author: 'Harry Hu',
+  email: 'hujiangtao1235@qq.com',
+  buildDev: false,
+  isDebug: false,
+  hotPostNum: 5,
+  sessionSecret: 'itdotaer',
+  hash: 'bb645107f2caa485cab1',
+  apiUrl: 'http://itdotaerblog.herokuapp.com/api',
+  gitHubRepo: 'http://github.com/Itdotaer/ItdotaerBlog'
 };
 
 var dbConfig = {
-    poolSize: 20,
-    address: 'mongodb://itdotaer:itdotaer12358@ds013414.mlab.com:13414/itdotaer'
+  poolSize: 20,
+  address: 'mongodb://itdotaer:itdotaer12358@ds013414.mlab.com:13414/itdotaer'
 }
 
 module.exports = {
-    appInfo: appInfo,
-    dbConfig: dbConfig
+  appInfo: appInfo,
+  dbConfig: dbConfig
 };
--- webpack.config.js
+++ webpack.config.js
@@ -8,68 +8,61 @@ var appInfo = require('./config').appInfo;
 
 //Deps
 var deps = [
-    'react/dist/react.min.js',
-    'react-dom/dist/react-dom.min.js',
-    'react-router/dist/react-router.min.js',
-    'react-bootstrap/dist/react-bootstrap.min.js',
-    'moment/min/moment.min.js',
-    'jquery/dist/jquery.min.js'
+  'react/dist/react.min.js',
+  'react-dom/dist/react-dom.min.js',
+  'react-router/dist/react-router.min.js',
+  'react-bootstrap/dist/react-bootstrap.min.js',
+  'moment/min/moment.min.js',
+  'jquery/dist/jquery.min.js'
 ];
 
 var config = {
   entry: {
-      app: path.resolve(__dirname, 'app/main.js'),
-      vendors: ['react', 'react-dom', 'react-router', 'react-bootstrap', 'moment', 'jquery', 'promise', 'history']
+    app: path.resolve(__dirname, 'app/main.js')
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: appInfo.buildDev ? '[name].js' : '[name]-[hash].js',
-    chunkFileName: appInfo.buildDev ? '[name].js' : '[name]-[chunkhash].js'
+    filename: appInfo.buildDev ? '[name].js' : '[name]-[hash].js'
   },
-  resolve:{
-      alias:{}
+  resolve: {
+    alias: {}
   },
   module: {
-    loaders:[
-        {
-            test: /\.jsx?$/, // js or jsx files
-            exclude: [node_modules],
-            loader: 'babel' // load babel-loader module
-        },
-        {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-        },
-        {
-            test: /\.less$/,
-            loader: ExtractTextPlugin.extract(
-                    // activate source maps via loader query
-                    'css?sourceMap!' +
-                    'less?sourceMap'
-                )
-        }
+    loaders: [
+      {
+        test: /\.jsx?$/, // js or jsx files
+        exclude: [node_modules],
+        loader: 'babel' // load babel-loader module
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract(
+          // activate source maps via loader query
+          'css?sourceMap!' +
+          'less?sourceMap'
+        )
+      }
     ],
     noParse: []
-},
-plugins: [
-    //vendors
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendors',
-      filename: appInfo.buildDev ? '[name].js' : '[name]-[hash].js',
-      minChunks: Infinity
-    }),
-
+  },
+  plugins: [
     //Css files
-     new ExtractTextPlugin(appInfo.buildDev ? '[name].css' : '[name]-[hash].css', {allChunks: true})
+    new ExtractTextPlugin(appInfo.buildDev ? '[name].css' : '[name]-[hash].css', {
+      allChunks: true
+    })
   ]
 };
 
 //No parse libs
-deps.forEach(function(dep){
-    var depPath = path.resolve(node_modules, dep);
+deps.forEach(function(dep) {
+  var depPath = path.resolve(node_modules, dep);
 
-    config.resolve.alias[dep.split(path.sep)[0]] = depPath;
-    config.module.noParse.push(depPath);
+  config.resolve.alias[dep.split(path.sep)[0]] = depPath;
+  config.module.noParse.push(depPath);
 });
 
 module.exports = config;
